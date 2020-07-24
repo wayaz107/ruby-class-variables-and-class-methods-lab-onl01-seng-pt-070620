05ffb1a0bbe146d7df8e564eb8a52ad47f788fc0
Warda Ayaz <wayaz107@gmail.com>
2020-07-24 18:20:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+
+attr_accessor :name :artist :genre
+
+@@count = 0
+@@artists = []
+@@genres = []
+
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@genre << @genre
+  @@artists << @artist
+@@count += 1
+end 
+
+def self.count
+  @@count
+end 
+
+def self.genres
+  @@genres
+end 
+
+def self.artists
+  @@artists
+end 
+
+def self.genre_count
+  @@genre_count = {}
+end 
+
+def self.artist_count
+  @artist_count = {}
+end 
+end  
