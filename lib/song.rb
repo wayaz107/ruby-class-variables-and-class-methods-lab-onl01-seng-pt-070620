4dfe621d9d049f1951232310c0061de0f1af2757
Warda Ayaz <wayaz107@gmail.com>
2020-07-24 18:26:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,18 +21,34 @@ def self.count
 end
 
 def self.genres
-  @@genres
+  @@genres.uniq
 end
 
 def self.artists
-  @@artists
+  @@artists.uniq
 end
 
 def self.genre_count
   @@genre_count = {}
+  @@genre_count.each do |genre|
+    if genre_count[genre] 
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end 
+    genre_count
 end
 
 def self.artist_count
-  @artist_count = {}
+  @@artist_count = {}
+  @@artist_count.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] +=1
+    else
+      artist_count[artist] = 1
+    end 
+    artist_count
+  end 
+  
 end
 end
